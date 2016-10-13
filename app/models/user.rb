45c56f1d61d2dec730bae7bbac82dbce472e0c4a
Daniel Palomino <dapalominop@gmail.com>
2016-10-13 15:34:51 -0500

    Hbailitación de token. se adherio username al user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+	# Include default devise modules. Others available are:
+	# :confirmable, :lockable, :timeoutable and :omniauthable
+	devise :database_authenticatable, :registerable,
+		:recoverable, :rememberable, :trackable, :validatable,
+		:confirmable
+
+	attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+	
 end
